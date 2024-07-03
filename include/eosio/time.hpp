c9375c72b25dee0f9f24446c0363b4145b1daa84
sirius4galaxy <uniswap_v2@163.com>
2024-07-03 18:23:06 +0800

    change block interval to 1000ms

--- include/eosio/time.hpp
+++ include/eosio/time.hpp
@@ -226,7 +226,7 @@ class block_timestamp {
    bool                     operator==(const block_timestamp& t) const { return slot == t.slot; }
    bool                     operator!=(const block_timestamp& t) const { return slot != t.slot; }
    uint32_t                 slot                  = 0;
-   static constexpr int32_t block_interval_ms     = 500;
+   static constexpr int32_t block_interval_ms     = 1000;
    static constexpr int64_t block_timestamp_epoch = 946684800000ll; // epoch is year 2000
                                                                     /// @endcond
  private:
